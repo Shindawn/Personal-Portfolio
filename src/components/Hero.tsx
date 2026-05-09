09453aea86a6d077c9a89486f57d33863215abe3
Shindawn <lgcaadlawon@catsu.edu.ph>
2026-05-09 23:52:11 +0800

    update resume

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
   // Resume viewer state
   const [isResumeOpen, setIsResumeOpen] = useState(false);
   const resumeOptions = [
-    { label: "Standard Format", url: "/resumes/new.pdf" },
+    { label: "Standard Format", url: "/resumes/resume.pdf" },
   ];
   const [selectedResumeIndex, setSelectedResumeIndex] = useState(0);
   const selectedResume = resumeOptions[selectedResumeIndex];
